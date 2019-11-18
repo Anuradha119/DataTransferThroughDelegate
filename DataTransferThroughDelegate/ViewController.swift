f68fd57171b419a8bc1ab9c99dd02acf5fb9e8d7
Marni Anuradha <marnianuradha@achiever-111.local>
2019-11-18 10:55:13 +0530

    Initial Commit

---
+++ DataTransferThroughDelegate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DataTransferThroughDelegate
+//
+//  Created by Marni Anuradha on 11/18/19.
+//  Copyright © 2019 Marni Anuradha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
